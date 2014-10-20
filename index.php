b8647175491df9f300f05631978a9c2c1c746ea7
tanapad <tanapado54@email.nu.ac.th>
2014-10-20 14:48:13 +0700

    edit index.php
    edit test code of index.php

--- index.php
+++ index.php
@@ -4,17 +4,10 @@
 	<meta charset='utf-8'>
 </head>
 <body>
-<<<<<<< HEAD
 <div id="login">
 	<?php 
 		require_once('login.php');
 	?>
 <div>
-=======
-	Group 2 & Friend
-	Hello world
-	Software
-	FUCK YOU
->>>>>>> origin/master
 </body>
 </html>
