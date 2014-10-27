caad1c9a9d75a3d68f5b698aac147f6f8e47f124
yanin11 <yanin.watta@gmial.com>
2014-10-27 23:33:44 +0700

    show subject of teacher

---
+++ showSubject.php
@@ -0,0 +1,14 @@
+<html>
+<head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" ></head>
+	<body>
+	<div align="center">
+		<br>
+			<table width="800" border=”1″>
+		<tr>
+		<td align = "center"><b>รหัสวิชา</b></td>
+		<td align = "center"><b>ชื่อวิชา</b></td>
+		<td align = "center"><b>หน่วยกิจ</b></td>
+		<td align = "center"><b>กลุ่มเรียน</b></td>
+		</tr>
+	</body>
+</html>
