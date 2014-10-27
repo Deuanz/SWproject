5701dc96346043c60507d97fbdb6b1bffd9d0d87
tanapad <tanapado54@email.nu.ac.th>
2014-10-27 17:28:46 +0700

    add profile student version mockup

---
+++ profile_student.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+	
+
+	
+	</head>
+	<style>
+		#sizep {
+		height:300px;
+		width:200px;
+	}
+	</style>
+	<body>
+				<div id="sizep" style="float:left;"> pic </div> 
+				<div style="float:left;">
+				 ชื่อ กี๊ดดดดดดดดดด นามสกุล สุดหล่อมาก <br>
+				มหาวิทยาลัย นเรศวร <br>
+				วิทยาเขต พิษณุโลก<br>
+				คณะ วิศวกรรมศาสตร์<br>
+				ภาควิชา วิศวกรรมไฟฟ้าและคอมพิวเตอร์<br>
+				หลักสูตร วิศวกรรมคอมพิวเตอร์<br>
+
+	</body>
+</html>
