b9bb50792571bfaa7835483c30c4373d91139c48
Rattanawadi <NooDeuan_CPENU@hotmail.com>
2014-11-02 18:49:34 +0700

    header after login

---
+++ mockup/header_logedin.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" href="css/ui.css">
+		<style>
+			#logedin-rigth{
+				float: right;
+				margin: 5 100 5 5;
+			}
+
+			#logedin-left{
+				float: left;
+				margin: 5 10;
+			}
+		</style>
+	</head>
+	<body>
+		<nav id="nav-top-logedin">
+			<div id="logedin-left"><img src = "..\image\logo2-mini2.png"> </div>
+			<div id="logedin-rigth">
+				สมชาย ใจดี(นักเรียน)
+
+				<input style="margin:0 10" type="submit" name="logout" value="Logout">
+			</div>
+		</nav>
+	</body>
+</html>
---
+++ mockup/slide-bar-student.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+
+	</head>
+
+	<body>
+		<div style="float: left; background: #000000">
+
+		</div>
+	</body>
+</html>
