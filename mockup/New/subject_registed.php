93590b697d527199d73d0054afe8b3fea4a0b0d3
Rattanawadi <NooDeuan_CPENU@hotmail.com>
2014-11-03 19:35:20 +0700

    add page student registed

---
+++ mockup/New/subject_registed.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<style>
+			table, td, th {
+	    		border: 1px solid black;
+	    		padding: 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div align="center">
+			<table width="100%" style="border-collapse: collapse">
+				<tr style="background:#999999; border:1px solid #000000;">
+					<td align = "center" width="15%">รหัสวิชา</td>
+					<td align = "center" width="60%">ชื่อวิชา</td>
+					<td align = "center" width="15%">ปีการศึกษา</td>
+					<td align = "center" width="10%">ดาวน์โหลด</td>
+				</tr>
+				<tr>
+					<td>000000</td>
+					<td>คอมพิวเตอร์ขั้นพื้นฐาน</td>
+					<td>1/2557</td>
+					<td align = "center"><a href="#"><img src = "icon/down123.png"></a></td>
+				</tr>
+				<tr>
+					<td>000000</td>
+					<td>คอมพิวเตอร์ขั้นพื้นฐาน</td>
+					<td>1/2557</td>
+					<td align = "center"><img src = "icon/blocking.png"></td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
