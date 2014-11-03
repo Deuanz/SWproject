177574dc50c4cf8da5cd68eeef618eb801665ced
yanin11 <yanin.watta@gmial.com>
2014-11-03 19:28:33 +0700

    form student profile

---
+++ mockup/New/student_profile.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<style>
+			table, td, th {
+	    		border: 2px solid white;
+	    		padding: 5px;
+			}
+			
+			
+		</style>
+		</head>
+<body bgcolor="#F8ECDE">
+
+	<table style="border-collapse: collapse">
+			<tr style="background:#ECAB53; border:2px">
+				<td WIDTH=512; align = "center"><b>ข้อมูลส่วนตัว</b></td>
+			</tr>
+			
+	</table>
+
+	<table style="border-collapse: collapse">
+			<tr>
+			<td WIDTH=150; align = ""><b>รหัสประจำตัว  :</b> </td> 
+			<td WIDTH=350; align = "">54360087 </td> 
+			</tr>
+			<tr>
+			<td WIDTH=150; align = ""><b>ชื่อ  :</b> </td> 
+			<td WIDTH=350; align = "">นางสาวญาณิน  วัฒนกุลชัย </td> 
+			</tr>
+			<tr>
+			<td WIDTH=150; align = ""><b>คณะ  :</b> </td> 
+			<td WIDTH=350; align = "">วิศวกรรมศาสตร์</td> 
+			</tr>
+			<tr>
+			<td WIDTH=150; align = ""><b>หลักสูตร  :</b> </td> 
+			<td WIDTH=350; align = "">วิศวกรรมคอมพิวเตอร์</td> 
+			</tr>
+			<tr>
+			<td WIDTH=150; align = ""><b>วิทยาเขต  :</b> </td> 
+			<td WIDTH=350; align = "">พิษณุโลก</td> 
+			</tr>
+			<tr>
+			<td WIDTH=150; align = ""><b>มหาวิทยาลัย  :</b> </td> 
+			<td WIDTH=350; align = "">นเรศวร</td> 
+			</tr>
+	</table>
+
+			
+				
+	
+	
+</body>
+</html>
