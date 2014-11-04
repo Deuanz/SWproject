15ffa8674055d5ad6bd5433f41d181dcc4874232
Rattanawadi <NooDeuan_CPENU@hotmail.com>
2014-11-04 09:17:18 +0700

    fix broder table registed student

--- mockup/New/subject_registed.php
+++ mockup/New/subject_registed.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset='utf-8'>
 		<style>
-			table, td, th {
+			#border-table{
 	    		border: 1px solid black;
 	    		padding: 10px;
 			}
@@ -10,24 +10,24 @@
 	</head>
 	<body>
 		<div align="center">
-			<table width="100%" style="border-collapse: collapse">
-				<tr style="background:#999999; border:1px solid #000000;">
-					<td align = "center" width="15%">รหัสวิชา</td>
-					<td align = "center" width="60%">ชื่อวิชา</td>
-					<td align = "center" width="15%">ปีการศึกษา</td>
-					<td align = "center" width="10%">ดาวน์โหลด</td>
+			<table id="border-table" width="100%" style="border-collapse: collapse">
+				<tr id="border-table"style="background:#999999; border:1px solid #000000;">
+					<td id="border-table" align = "center" width="15%">รหัสวิชา</td>
+					<td id="border-table" align = "center" width="60%">ชื่อวิชา</td>
+					<td id="border-table" align = "center" width="15%">ปีการศึกษา</td>
+					<td id="border-table" align = "center" width="10%">ดาวน์โหลด</td>
 				</tr>
-				<tr>
-					<td>000000</td>
-					<td>คอมพิวเตอร์ขั้นพื้นฐาน</td>
-					<td>1/2557</td>
-					<td align = "center"><a href="#"><img src = "icon/down123.png"></a></td>
+				<tr id="border-table">
+					<td id="border-table">000000</td>
+					<td id="border-table">คอมพิวเตอร์ขั้นพื้นฐาน</td>
+					<td id="border-table">1/2557</td>
+					<td id="border-table" align = "center"><a href="#"><img src = "icon/down123.png"></a></td>
 				</tr>
-				<tr>
-					<td>000000</td>
-					<td>คอมพิวเตอร์ขั้นพื้นฐาน</td>
-					<td>1/2557</td>
-					<td align = "center"><img src = "icon/blocking.png"></td>
+				<trid="border-table" >
+					<td id="border-table">000001</td>
+					<td id="border-table">คอมพิวเตอร์ขั้นสูง</td>
+					<td id="border-table">2/2557</td>
+					<td id="border-table" align = "center"><img src = "icon/blocking.png"></td>
 				</tr>
 			</table>
 		</div>
